ccb986b0efbb049b941cd440272a20dbc7167e5a
rashid awan <rashid.awan@startrum.com>
2019-07-04 12:33:05 +0500

    fix ui

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,61 @@
 <template>
     <div id="app">
-        <div id="nav">
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link>
+        <div class="main-content">
+            <router-view />
         </div>
-        <router-view />
     </div>
 </template>
 
 <style lang="scss">
 @import 'assets/scss/app.scss';
 </style>
+<script>
+export default {
+    components: {},
+    /*
+        |--------------------------------------------------------------------------
+        | Component > props
+        |--------------------------------------------------------------------------
+        */
+    props: {
+        /**
+         * Value to determine the current compose mode which
+         * varies between 'add' and 'edit'
+         */
+        mode: {
+            type: String,
+            default: 'add',
+        },
+    }, // End of Component > props
+
+    /*
+        |--------------------------------------------------------------------------
+        | Component > data
+        |--------------------------------------------------------------------------
+        */
+    data() {
+        return {}
+    }, // End of Component > data
+
+    /*
+        |--------------------------------------------------------------------------
+        | Component > computed
+        |--------------------------------------------------------------------------
+        */
+    computed: {}, // End of Component > computed
+
+    /*
+        |--------------------------------------------------------------------------
+        | Component > methods
+        |--------------------------------------------------------------------------
+        */
+    methods: {}, // End of Component > methods
+
+    /*
+        |--------------------------------------------------------------------------
+        | Component > mounted
+        |--------------------------------------------------------------------------
+        */
+    mounted() {}, // End of Component > mounted
+} // End of export default
+</script>
