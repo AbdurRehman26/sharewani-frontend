8fd69353cee88f33392c57d2c4cac3b45ecd5092
rashid awan <rashid.awan@startrum.com>
2019-07-05 09:46:31 +0500

    create filter component

---
+++ src/views/profile/Main.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="dashboard">
+        <base-sidebar></base-sidebar>
+        <div class="content-area">
+            <base-header :menuListing="menuListing"></base-header>
+            <div class="right-panel">
+                <filter-panel
+                    :filterList="options"
+                    :filterSelected="selected"
+                    filterHeading="All Customers"
+                ></filter-panel>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import filterPanel from '@/components/FilterPanel.vue'
+export default {
+    components: {
+        filterPanel,
+    },
+
+    /*
+    |--------------------------------------------------------------------------
+    | Component > props
+    |--------------------------------------------------------------------------
+    */
+    props: {
+        /**
+         * Value to determine the current compose mode which
+         * varies between 'add' and 'edit'
+         */
+        mode: {
+            type: String,
+            default: 'add',
+        },
+    }, // End of Component > props
+
+    /*
+    |--------------------------------------------------------------------------
+    | Component > data
+    |--------------------------------------------------------------------------
+    */
+    data() {
+        return {
+            menuListing: [
+                {
+                    anchorLink: '/',
+                    menuLabel: 'Alerts',
+                },
+                {
+                    anchorLink: '/',
+                    menuLabel: 'Activity Dashboard',
+                },
+                {
+                    anchorLink: '/',
+                    menuLabel: 'Customer Statistics',
+                },
+            ],
+
+            selected: '1',
+
+            options: [
+                { text: 'All', value: '1' },
+                { text: 'Pending Review', value: '2' },
+                { text: 'Approved', value: '3' },
+                { text: 'Rejected', value: '4' },
+            ],
+        }
+    }, // End of Component > data
+
+    /*
+    |--------------------------------------------------------------------------
+    | Component > computed
+    |--------------------------------------------------------------------------
+    */
+    computed: {}, // End of Component > computed
+
+    /*
+    |--------------------------------------------------------------------------
+    | Component > methods
+    |--------------------------------------------------------------------------
+    */
+    methods: {}, // End of Component > methods
+
+    /*
+    |--------------------------------------------------------------------------
+    | Component > mounted
+    |--------------------------------------------------------------------------
+    */
+    mounted() {}, // End of Component > mounted
+} // End of export default
+</script>
