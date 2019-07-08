fec4c2418ca4856872fb3a3f95cd3377370617d0
imran <imran.azhar@cygnismedia.com>
2019-07-08 02:44:53 -0700

    auth router seeting

--- src/router.js
+++ src/router.js
@@ -11,26 +11,36 @@ export default new Router({
             path: '/',
             name: 'login-page',
             component: () =>
-                import(/* webpackChunkName: "login" */ '@/views/auth/Login.vue'),
+                import(/* webpackChunkName: "login" */ '@/views/auth/Main.vue'),
+
+            children: [
+                {
+                    path: '/',
+                    name: 'login-page',
+                    component: () =>
+                        import(/* webpackChunkName: "forgot-password" */ '@/views/auth/Login.vue'),
+                },
+                {
+                    path: '/forgot-password',
+                    name: 'forgot-page',
+                    component: () =>
+                        import(/* webpackChunkName: "forgot-password" */ '@/views/auth/ForgotPassword.vue'),
+                },
+                {
+                    path: '/reset-password',
+                    name: 'reset-password-page',
+                    component: () =>
+                        import(/* webpackChunkName: "reset-password" */ '@/views/auth/ResetPassword.vue'),
+                },
+                {
+                    path: '/email-sent',
+                    name: 'email-sent-page',
+                    component: () =>
+                        import(/* webpackChunkName: "email-sent" */ '@/views/auth/EmailSent.vue'),
+                },
+            ],
         },
-        {
-            path: '/forgot-password',
-            name: 'forgot-page',
-            component: () =>
-                import(/* webpackChunkName: "forgot-password" */ '@/views/auth/ForgotPassword.vue'),
-        },
-        {
-            path: '/reset-password',
-            name: 'reset-password-page',
-            component: () =>
-                import(/* webpackChunkName: "reset-password" */ '@/views/auth/ResetPassword.vue'),
-        },
-        {
-            path: '/email-sent',
-            name: 'email-sent-page',
-            component: () =>
-                import(/* webpackChunkName: "email-sent" */ '@/views/auth/EmailSent.vue'),
-        },
+
         {
             path: '/contact-us',
             name: 'contact-us-page',
