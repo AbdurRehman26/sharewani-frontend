e0dc86a669c622d266520fb4978860d797c2ba85
Bakhtiar <bakhtiar.aleem@startrum.com>
2019-07-11 14:25:00 +0500

    config import data page

---
+++ src/components/CardData.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="card">
+        <div
+            class="logo"
+            :style="{
+                'background-image':
+                    'url(' +
+                    require('@/assets/images/data-image/' + logo + '') +
+                    ')',
+            }"
+        ></div>
+        <h4>{{ name }}</h4>
+        <p>{{ lastUpdate }}</p>
+        <p>{{ totalRecord }}</p>
+        <b-button @click="$emit('updateData')" variant="primary"
+            >Update Data Set</b-button
+        >
+        <b-button @click="$emit('currentData')" variant="link"
+            ><i class="icon-cloud"></i> Download Current Data Set</b-button
+        >
+    </div>
+</template>
+<script>
+export default {
+    /*
+        |--------------------------------------------------------------------------
+        | Component > props
+        |--------------------------------------------------------------------------
+        */
+    props: {
+        logo: {
+            type: String,
+            default: null,
+        },
+        name: {
+            type: String,
+            default: null,
+        },
+
+        lastUpdate: {
+            type: String,
+            default: null,
+        },
+
+        totalRecord: {
+            type: String,
+            default: null,
+        },
+    }, // End of Component > props
+
+    /*
+        |--------------------------------------------------------------------------
+        | Component > data
+        |--------------------------------------------------------------------------
+        */
+    data() {
+        return {}
+    }, // End of Component > data
+
+    /*
+        |--------------------------------------------------------------------------
+        | Component > computed
+        |--------------------------------------------------------------------------
+        */
+    computed: {}, // End of Component > computed
+
+    /*
+        |--------------------------------------------------------------------------
+        | Component > methods
+        |--------------------------------------------------------------------------
+        */
+    methods: {}, // End of Component > methods
+
+    /*
+        |--------------------------------------------------------------------------
+        | Component > mounted
+        |--------------------------------------------------------------------------
+        */
+    mounted() {}, // End of Component > mounted
+} // End of export default
+</script>
--- src/views/configuration/ImportData.vue
+++ src/views/configuration/ImportData.vue
@@ -7,12 +7,32 @@
                 </div>
             </div>
         </div>
+
+        <div class="data-import">
+            <div class="row">
+                <div
+                    class="col-lg-4 col-md-6"
+                    v-for="(data, index) in importData"
+                    :key="index"
+                >
+                    <card-data
+                        :logo="data.logo"
+                        :name="data.name"
+                        :lastUpdate="data.lastUpdate"
+                        :totalRecord="data.totalRecord"
+                    ></card-data>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import cardData from '@/components/CardData.vue'
 export default {
-    components: {},
+    components: {
+        cardData,
+    },
 
     /*
     |--------------------------------------------------------------------------
@@ -36,7 +56,28 @@ export default {
     |--------------------------------------------------------------------------
     */
     data() {
-        return {}
+        return {
+            importData: [
+                {
+                    logo: 'logo1.png',
+                    name: 'Do Jones Data',
+                    lastUpdate: 'Last updated: June 10, 2019',
+                    totalRecord: 'Total Records: 45,650',
+                },
+                {
+                    logo: 'logo2.png',
+                    name: 'World Bank Data',
+                    lastUpdate: 'Last updated: June 9, 2019',
+                    totalRecord: 'Total Records: 23,956',
+                },
+                {
+                    logo: 'logo3.png',
+                    name: 'Client Blacklist Data',
+                    lastUpdate: 'Last updated: June 12, 2019',
+                    totalRecord: 'Total Records: 2,450',
+                },
+            ],
+        }
     }, // End of Component > data
 
     /*
