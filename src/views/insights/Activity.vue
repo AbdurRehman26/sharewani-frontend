f9b8cb2f618b6aec6b6cdb9cdb97e56dc9a7d060
Bakhtiar <bakhtiar.aleem@startrum.com>
2019-08-26 12:34:49 +0500

    datepicker changes related to pages

--- src/views/insights/Activity.vue
+++ src/views/insights/Activity.vue
@@ -12,9 +12,7 @@
                                 <date-time-picker
                                     dateFormat="ll"
                                     placeholder="Start date"
-                                    date
-                                    close
-                                    noFooter
+                                    formatVal="DD-MM-YYYY"
                                 ></date-time-picker>
                             </div>
                         </div>
@@ -23,9 +21,7 @@
                                 <date-time-picker
                                     dateFormat="ll"
                                     placeholder="End date"
-                                    date
-                                    close
-                                    noFooter
+                                    formatVal="DD-MM-YYYY"
                                 ></date-time-picker>
                             </div>
                         </div>
